f27f82a00fc176cea13eedb84ecdc7f5a4599f7c
SamJust <samjustanimstudio@gmail.com>
2018-08-10 09:34:52 +0300

    fix

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./config.json');
+const request = require('request-promise-native');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
